2cd7a3d41beb18af25fde32993cd1494117dc714
hugo bosse <monsieurbosse@gmail.com>
2017-03-16 13:45:29 +0100

    working on concept

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :team ]
+  skip_before_action :authenticate_user!, only: [ :home, :team, :concept ]
 
   def home
   end
 
   def team
+  end
+
+  def concept
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/team/', to: 'pages#team', as: 'team'
+  get '/valeurs/', to: 'pages#concept', as: 'concept'
 
 end
