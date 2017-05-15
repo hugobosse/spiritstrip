cfe6d2d63157967e361dc2ea27ca717b0bd16e3d
hugo bosse <monsieurbosse@gmail.com>
2017-05-15 16:14:34 +0200

    modified blog controller

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,10 @@
 class BlogController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :blog]
+  skip_before_action :authenticate_user!, only: [ :post, :blog]
 
   def post
     render "/#{self.controller_name}/#{params[:title]}/index.html"
+  end
+
+  def blog
   end
 end
