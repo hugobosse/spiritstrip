7d46944f568b94a4e7beb6b83445bae59669f92e
hugo bosse <monsieurbosse@gmail.com>
2017-05-15 16:29:03 +0200

    trying without authenticate user

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,4 @@
 class BlogController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :post, :blog]
 
   def post
     render "/#{self.controller_name}/#{params[:title]}/index.html"
