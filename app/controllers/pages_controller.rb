b1c58bea4db32bb8e85fc00bf785437f49e223a6
hugo bosse <monsieurbosse@gmail.com>
2017-05-15 15:43:19 +0200

    trying with https for photos and trying issues solution from gdub

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :team, :concept ]
+  skip_before_action :authenticate_user!, only: [ :home, :team, :concept, :blog ]
 
   def home
   end
@@ -8,5 +8,8 @@ class PagesController < ApplicationController
   end
 
   def concept
+  end
+
+  def blog
   end
 end
