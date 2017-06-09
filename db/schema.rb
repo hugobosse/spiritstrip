8320d398c530f3743abdd041042d6a4fd869fd4f
hugo bosse <monsieurbosse@gmail.com>
2017-06-09 14:16:03 +0200

    recap de command order#show et seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606203632) do
+ActiveRecord::Schema.define(version: 20170609113847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20170606203632) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false, null: false
+    t.date     "age"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Category.destroy_all
+Size.destroy_all
+Product.destroy_all
+
+puts 'Creating categories and Size...'
+whisky = Category.create!(name: 'whisky')
+bouteille = Size.create!(name: '70cl', volume: '70cl')
+
+puts 'Creating products...'
+Product.create!(sku: 'AT001W',   name: 'Auchentoshan American Oak', title:'Le Bon', category: whisky, size: bouteille, image: 'whisky_auchentoshan.png', price_cents: 3000, note: 'Vanille, Noix de Coco, Pamplemousse', description: 'Ce whisky des Lowlands, vieilli exclusivement en ex fûts de Bourbon est le cadeau parfait pour les papas GENTILS. A la fois doux et délicat Auchentoshan American Oak définit à lui seul les arômes habituels de ce mode de maturation.')
+Product.create!(sku: 'AB010W',   name: 'Ardberg Ten', title:'La Brute', category: whisky, size: bouteille, image: 'ardberg_ten.png', price_cents: 4000, note: 'Hareng fumé (tourbe), Cacao, Ananas', description: 'Ce « monstre » de tourbe ravira les palais des plus experts comme ceux des novices. LE cadeau idéal pour les papas COSTAUD. En provenance de l’île d’Islay Ardbeg Ten résume parfaitement ces lieux à la fois rudes et accueillant. En bref, l’équilibre parfait !')
+Product.create!(sku: 'GF105W',   name: 'Glenfarclas 105', title:'Le Truand', category: whisky, size: bouteille, image: 'glenfarclas_105.png', price_cents: 6900, note: 'Sherry, Noisette, Épices', description: 'Comment faire passer un Single Cask à 60% pour du « petit lait » ? C’est la question qui définit le charme de ce whisky du Speyside vieilli en fût de Sherry (vin de Xéres). Glenfarclas 105 (prononcer « One O Five ») est sans conteste le cadeau parfait pour le papa MALIN.')
+puts 'Finished!'
